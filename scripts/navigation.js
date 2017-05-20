048f39bf0bc965a92a72544f2f463f0d1497604c
Andrzej Ładomirski <ladomirski@gmail.com>
2017-05-20 12:33:08 +0200

    script fixed

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         $('.navBar__body').toggleClass('open');
     });
 
-    $('#aboutBtn, #frontBtn, #musicBtn, #hobbyBtn, #contactBtn').click(function() {
+    $('#aboutBtn, #frontBtn, #musicBtn, #hobbyBtn, #contactBtn, #languageBtn').click(function() {
         $('.navBar__header--menuBtnIcon').toggleClass('open');
         $('.navBar__body').toggleClass('open');
     });
